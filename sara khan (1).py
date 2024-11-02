97c25c86626cfb338455d9424cba4cfd90fa599a
Sarakhan19 <sarasawat8090@gmail.com>
2024-11-03 00:14:57 +0500

    Add files via upload

---
+++ sara khan (1).py
@@ -0,0 +1,38 @@
+number1=9
+number2=15
+number3=17
+
+# calculate sum
+sum=number1+number2+number3
+print("sum:",sum)
+
+# calculate difference
+diffference=number1-number2-number3
+print("difference:",diffference)
+
+# calculate product
+product=number1*number2*number3
+print("product:",product)
+
+# calculate average
+average=(number1+number2+number3)
+print("average:",average)
+#area of triangle
+breathe=8
+height=4
+area=0.5*breathe*height
+print("triangle area",area)
+#area of square
+side=3
+area=side*side
+print("square area",area)
+#area of circle
+p1=3.14
+radius=5
+area=p1*radius*radius
+print("circle area",area)
+#rectangle area
+length=10
+width=9
+area=length*width
+print("rectangle area",area)
